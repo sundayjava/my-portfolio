034d70116c3f451faf6afb7ea8c6a58d52b22b54
sundayjava <sundaydavidudoekong@gmail.com>
2024-01-18 15:24:27 +0100

    scrolltop

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Navbar } from "./components/Navbar";
 import Hero from "./components/Hero";
 import Project from "./components/Project";
@@ -8,6 +8,31 @@ import Footer from "./components/Footer";
 
 const App = () => {
   const [darkTheme, setDarkTheme] = useState(true);
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    // Add a scroll event listener to track the scroll position
+    const handleScroll = () => {
+      // Show the button if the user has scrolled down 100 pixels or more
+      setIsVisible(window.scrollY > 100);
+    };
+
+    // Attach the event listener
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener when the component is unmounted
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    // Scroll to the top of the page when the button is clicked
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth", // Add smooth scrolling animation
+    });
+  };
 
   return (
     <div className={darkTheme ? "dark" : ""}>
@@ -18,6 +43,17 @@ const App = () => {
         <Project />
         <Contact />
         <Footer />
+        <div>
+          {isVisible && (
+            <button
+              onClick={scrollToTop}
+              className="bg-white text-gray-900 font-bold text-[14px] scroll-smooth p-5 rounded-[100%]"
+              style={{ position: "fixed", bottom: "20px", right: "20px" }}
+            >
+              Top
+            </button>
+          )}
+        </div>
       </div>
     </div>
   );
